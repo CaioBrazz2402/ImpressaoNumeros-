94fc806a0dbc3c43c6e4051ee5ca7f63d3c37300
CaioBrazz2402 <caiolima8133@gmail.com>
2025-04-28 22:59:07 -0300

    Adiciona estrutura do projeto Java (pom.xml, src, target)

---
+++ src/main/java/com/mycompany/impressaonumeros/ImpressaoNumeros.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.impressaonumeros;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author CaioBraz
+ */
+public class ImpressaoNumeros {
+
+    public static void main(String[] args) {
+        int numero = 1;
+        
+        while (numero <= 50) {
+            System.out.println(numero);
+            numero++;
+        }
+    }
+}
+
+  
